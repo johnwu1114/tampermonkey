4f822cde59c506eab5c402c5f529028942bdd16c
John Wu <me@johnwu.cc>
2024-07-10 23:15:12 +0800

    Update common.js

--- scripts/common.js
+++ scripts/common.js
@@ -3,20 +3,19 @@ const common = {
         const markdownBody = $("div.kbnMarkdown__body");
         if (!markdownBody.length) return false;
 
-        version = version.split(".").map(x => x.padStart(10, "0")).join("");
         let mdScriptName = "";
         let mdVersion = "";
         markdownBody.find("code").each((_, code) => {
             const text = $(code).text().trim();
             if (text.indexOf("version:") !== -1) {
-                mdVersion = text.replace("version:", "").trim().split(".").map(x => x.padStart(10, "0")).join("");
+                mdVersion = text.replace("version:", "").trim();
             } else if (text.replace("script:", "").trim() === scriptName) {
                 mdScriptName = scriptName
             }
         });
 
         const enabled = mdScriptName == scriptName;
-        if (enabled && mdVersion > version) {
+        if (enabled && this.padVersion(mdVersion) > this.padVersion(version)) {
             markdownBody.append(
                 `<h2 style='background-color:yellow'>Update the ${scriptName} script to ${mdVersion} or above.</h2>` +
                 "Follow the <a target='_blank' href='https://github.com/johnwu1114/tampermonkey?tab=readme-ov-file#update-script'>document</a> to perform the update."
@@ -37,5 +36,8 @@ const common = {
         if (outcome === -0.25) return stake / 2;
         if (outcome <= -0.5) return stake;
         return 0;
+    },
+    padVersion(version) {
+        return version.split(".").map(x => x.padStart(10, "0")).join("");
     }
 };
